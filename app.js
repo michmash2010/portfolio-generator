1767c2b6a282479ef3112c31a08635df949095f9
michmash2010 <michellecrowe2010@gmail.com>
2021-10-13 19:24:57 -0400

    add function to generate HTML and modularize

--- app.js
+++ app.js
@@ -1,27 +1,14 @@
-// var message = 'Hello Node!';
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-// var sum = 5 + 3;
+const profileDataArgs = process.argv.slice(2);
 
-// console.log(message);
-// console.log(sum);
+const [name, github] = profileDataArgs;
 
-// var commandLineArgs = process.argv;
-// console.log(commandLineArgs);
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
 
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
-
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i++) {
-    console.log(profileDataArr[i]);
-    }
-
-    console.log('================');
-
-    // Is the same as this...
-    profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
